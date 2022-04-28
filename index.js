6af8117e61270f5ad3b8c3115280ada0a4dc700d
cwitmer34 <54679947+cwitmer34@users.noreply.github.com>
2022-04-28 19:21:23 -0400

    Reversed Question Order

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for (let i = annualExpenseQuestions.length - 1; i >= 0; i--) {
+    annualExpenses += parseFloat(window.prompt(annualExpenseQuestions[i]))
+} 
+
+for (let i = monthlyExpenseQuestions.length - 1; i >= 0; i--) {
+  monthlyExpenses += parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+}
+
+for (let i = weeklyExpenseQuestions.length - 1; i >= 0; i--) {
+    weeklyExpenses += parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+}
